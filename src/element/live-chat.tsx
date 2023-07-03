2cdf88339afb539824d9133eb9fd63d0fd3b6212
Alejandro Gomez <alejandro@strike.me>
2023-07-03 14:02:15 +0200

    feat: emoji reactions

--- src/element/live-chat.tsx
+++ src/element/live-chat.tsx
@@ -16,8 +16,10 @@ import {
   type ChangeEvent,
   type LegacyRef,
 } from "react";
-import { useHover } from "usehooks-ts";
+import { useHover, useOnClickOutside, useMediaQuery } from "usehooks-ts";
 
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
 import useEmoji from "hooks/emoji";
 import { System } from "index";
 import { useLiveChatFeed } from "hooks/live-chat";
@@ -141,6 +143,10 @@ function emojifyReaction(reaction: string) {
   return reaction;
 }
 
+interface Emoji {
+  native: string;
+}
+
 function ChatMessage({
   streamer,
   ev,
@@ -153,7 +159,10 @@ function ChatMessage({
   reactions: readonly TaggedRawEvent[];
 }) {
   const ref = useRef(null);
-  const isHovering = useHover(ref);
+  const emojiRef = useRef(null);
+  const isTablet = useMediaQuery("(max-width: 1020px)");
+  const [showZapDialog, setShowZapDialog] = useState(false);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const profile = useUserProfile(System, ev.pubkey);
   const zapTarget = profile?.lud16 ?? profile?.lud06;
   const zaps = reactions
@@ -166,45 +175,53 @@ function ChatMessage({
       .map((ev) => emojifyReaction(ev.content));
     return [...new Set(emojified)];
   }, [ev, reactions]);
+
   const hasReactions = emojis.length > 0;
   const totalZaps = useMemo(() => {
     const messageZaps = zaps.filter((z) => z.event === ev.id);
     return messageZaps.reduce((acc, z) => acc + z.amount, 0);
   }, [reactions, ev]);
   const hasZaps = totalZaps > 0;
+
+  useOnClickOutside(ref, () => {
+    setShowZapDialog(false);
+  });
+
+  useOnClickOutside(emojiRef, () => {
+    setShowEmojiPicker(false);
+  });
+
+  async function onEmojiSelect(emoji: Emoji) {
+    setShowEmojiPicker(false);
+    setShowZapDialog(false);
+    try {
+      const pub = await EventPublisher.nip7();
+      const reply = await pub?.generic((eb) => {
+        eb.kind(EventKind.Reaction)
+          .content(emoji.native)
+          .tag(["e", ev.id])
+          .tag(["p", ev.pubkey]);
+        return eb;
+      });
+      if (reply) {
+        console.debug(reply);
+        System.BroadcastEvent(reply);
+      }
+    } catch (error) {}
+  }
+
+  // @ts-expect-error
+  const topOffset = ref.current?.getBoundingClientRect().top;
+  // @ts-expect-error
+  const leftOffset = ref.current?.getBoundingClientRect().left;
+
   return (
     <>
       <div
         className={`message${link.author === ev.pubkey ? " streamer" : ""}`}
         ref={ref}
+        onClick={() => setShowZapDialog(true)}
       >
-        {zapTarget && (
-          <SendZapsDialog
-            lnurl={zapTarget}
-            aTag={
-              streamer === ev.pubkey
-                ? `${link.kind}:${link.author}:${link.id}`
-                : undefined
-            }
-            eTag={ev.id}
-            pubkey={ev.pubkey}
-            button={
-              isHovering ? (
-                <div className="message-zap-container">
-                  <button className="message-zap-button">
-                    <Icon
-                      name="zap-filled"
-                      className="message-zap-button-icon"
-                    />
-                  </button>
-                </div>
-              ) : (
-                <></>
-              )
-            }
-            targetName={profile?.name || ev.pubkey}
-          />
-        )}
         <Profile pubkey={ev.pubkey} />
         <Text content={ev.content} tags={ev.tags} />
         {(hasReactions || hasZaps) && (
@@ -222,7 +239,74 @@ function ChatMessage({
             ))}
           </div>
         )}
+        {ref.current && (
+          <div
+            className="message-zap-container"
+            style={
+              isTablet
+                ? {
+                    display: showZapDialog ? "flex" : "none",
+                  }
+                : {
+                    position: "fixed",
+                    top: topOffset - 12,
+                    left: leftOffset - 32,
+                    opacity: showZapDialog ? 1 : 0,
+                  }
+            }
+          >
+            {zapTarget && (
+              <SendZapsDialog
+                lnurl={zapTarget}
+                aTag={
+                  streamer === ev.pubkey
+                    ? `${link.kind}:${link.author}:${link.id}`
+                    : undefined
+                }
+                eTag={ev.id}
+                pubkey={ev.pubkey}
+                button={
+                  <button className="message-zap-button">
+                    <Icon name="zap" className="message-zap-button-icon" />
+                  </button>
+                }
+                targetName={profile?.name || ev.pubkey}
+              />
+            )}
+            <button
+              className="message-zap-button"
+              onClick={() => setShowEmojiPicker(true)}
+            >
+              <Icon name="face" className="message-zap-button-icon" />
+            </button>
+          </div>
+        )}
       </div>
+      {showEmojiPicker && (
+        <div
+          style={{
+            position: "fixed",
+            top: topOffset - 310,
+            left: leftOffset,
+            zIndex: 1,
+          }}
+          ref={emojiRef}
+        >
+          <style>
+            {`
+            em-emoji-picker { max-height: 300px; }
+            `}
+          </style>
+          <Picker
+            data={data}
+            perLine={7}
+            previewPosition="none"
+            skinTonePosition="search"
+            theme="dark"
+            onEmojiSelect={onEmojiSelect}
+          />
+        </div>
+      )}
     </>
   );
 }
