b96002abe92f46ccb8cb8eb6685f145b6b91d536
Kieran <kieran@harkin.me>
2023-08-28 15:19:20 +0100

    bugfix view count

--- src/pages/widgets/views.tsx
+++ src/pages/widgets/views.tsx
@@ -4,7 +4,7 @@ import { FormattedMessage } from "react-intl";
 import { findTag } from "utils";
 
 export function Views({ link }: { link: NostrLink }) {
-    const current = useCurrentStreamFeed(link);
+    const current = useCurrentStreamFeed(link, true);
 
     const viewers = findTag(current, "current_participants");
     return <div className="views">
