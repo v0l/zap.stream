576c9a2b9eae905fa08965031f433884dce255ec
Kieran <kieran@harkin.me>
2024-01-10 14:39:39 +0000

    chore: Update translations

--- functions/[id].ts
+++ functions/[id].ts
@@ -5,15 +5,18 @@ export const onRequest: PagesFunction<Env> = async context => {
 
   const next = await context.next();
   try {
-    const rsp = await fetch(`http://nostr.api.v0l.io/api/v1/opengraph/${id}?canonical=${encodeURIComponent("https://zap.stream/%s")}`, {
-      method: "POST",
-      body: await next.arrayBuffer(),
-      headers: {
-        "user-agent": "zap.stream/1.0 (https://zap.stream)",
-        "content-type": "text/html",
-        "accept": "text/html"
-      },
-    });
+    const rsp = await fetch(
+      `http://nostr.api.v0l.io/api/v1/opengraph/${id}?canonical=${encodeURIComponent("https://zap.stream/%s")}`,
+      {
+        method: "POST",
+        body: await next.arrayBuffer(),
+        headers: {
+          "user-agent": "zap.stream/1.0 (https://zap.stream)",
+          "content-type": "text/html",
+          accept: "text/html",
+        },
+      }
+    );
     if (rsp.ok) {
       const body = await rsp.text();
       if (body.length > 0) {
