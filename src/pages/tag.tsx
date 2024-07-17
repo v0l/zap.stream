52fb0c286231eacae16c571c9434a70b01c526b9
kieran <kieran@harkin.me>
2024-07-17 15:53:48 +0100

    fix: remove follow tag button

--- src/pages/tag.tsx
+++ src/pages/tag.tsx
@@ -1,6 +1,4 @@
 import { useParams } from "react-router-dom";
-import { unwrap } from "@snort/shared";
-import { FollowTagButton } from "@/element/follow-button";
 import { useStreamsFeed } from "@/hooks/live-streams";
 import VideoGridSorted from "@/element/video-grid-sorted";
 
@@ -11,7 +9,6 @@ export function TagPage() {
     <div>
       <div className="flex items-center justify-between">
         <h1>#{tag}</h1>
-        <FollowTagButton tag={unwrap(tag)} />
       </div>
       <VideoGridSorted evs={streams} />
     </div>
