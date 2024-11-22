4936e79aa37a3df2c51715f7ca9aef1cba661667
kieran <kieran@harkin.me>
2024-11-22 16:40:20 +0000

    chore: bump pkgs

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./fonts/outfit/outfit.css";
 
 import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
-import { NostrSystem } from "@snort/system";
+import { EventBuilder, NostrSystem } from "@snort/system";
 import { SnortContext } from "@snort/system-react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
@@ -47,6 +47,7 @@ const hasWasm = "WebAssembly" in globalThis;
 const workerRelay = new WorkerRelayInterface(
   import.meta.env.DEV ? new URL("@snort/worker-relay/dist/esm/worker.mjs", import.meta.url) : new WorkerVite(),
 );
+EventBuilder.ClientTag = ["client", "zap.stream", __ZAP_STREAM_VERSION__]
 const System = new NostrSystem({
   optimizer: hasWasm ? WasmOptimizer : undefined,
   cachingRelay: workerRelay,
