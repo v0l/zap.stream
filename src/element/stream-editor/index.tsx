4bade72dd67314ad90e979b51e7756eb4ee5c697
Eric P <ericpp@gmail.com>
2025-01-06 19:48:20 -0600

    Fix hour offset issue between date picker (in local time) and valueAsNumber (in UTC)

--- src/element/stream-editor/index.tsx
+++ src/element/stream-editor/index.tsx
@@ -206,7 +206,7 @@ export function StreamEditor({ ev, onFinish, options }: StreamEditorProps) {
                 type="datetime-local"
                 value={`${startsDate.getFullYear().toString().padStart(4, "0")}-${(startsDate.getMonth() + 1).toString().padStart(2, "0")}-${startsDate.getDate().toString().padStart(2, "0")}T${startsDate.getHours().toString().padStart(2, "0")}:${startsDate.getMinutes().toString().padStart(2, "0")}`}
                 onChange={e => {
-                  setStart((e.target.valueAsNumber / 1000).toString());
+                  setStart((new Date(e.target.value) / 1000).toString());
                 }}
               />
             </StreamInput>
