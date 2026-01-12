72a2be1b976ee9ce2dfaa84938fabde955506bac
Kieran <kieran@harkin.me>
2026-01-12 12:06:12 +0000

    chore: disable visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
-import { visualizer } from "rollup-plugin-visualizer";
+//import { visualizer } from "rollup-plugin-visualizer";
 import { vitePluginVersionMark } from "vite-plugin-version-mark";
 
 export default defineConfig({
@@ -27,11 +27,11 @@ export default defineConfig({
       command: "git describe --always --tags",
       ifMeta: false,
     }),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      filename: "build/stats.html",
-    }),
+    // visualizer({
+    //   open: true,
+    //   gzipSize: true,
+    //   filename: "build/stats.html",
+    // }),
   ],
   assetsInclude: ["**/*.md", "**/*.wasm"],
   build: {
